cd90dc43f8b3e7553f4dc61c85e9a44a8e4f844f
rustam <49564359+rustam76@users.noreply.github.com>
2024-08-12 05:50:48 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  plugins: [vercel()],
+  server: {
+    port: process.env.PORT as unknown as number,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
